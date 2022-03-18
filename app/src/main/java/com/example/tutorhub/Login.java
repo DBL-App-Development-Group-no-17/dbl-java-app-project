4cb99f123eae43518b74e5334c48b44044d2e5e3
TimStapert <82938308+TimStapert@users.noreply.github.com>
2022-03-18 11:15:51 +0100

    Register now made clickable

--- app/src/main/java/com/example/tutorhub/Login.java
+++ app/src/main/java/com/example/tutorhub/Login.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,6 +18,13 @@ public class Login extends AppCompatActivity {
         btn_login.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+        TextView txt_register = findViewById(R.id.register_txt);
+        txt_register.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+              //  startActivity(new Intent(getApplicationContext(), RegistrationPage.class));
             }
         });
     }
