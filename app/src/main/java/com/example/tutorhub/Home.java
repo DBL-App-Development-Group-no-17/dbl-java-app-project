d3eea88ec3129014e2df9146369163004a685645
ileshyadav <96896648+ileshyadav@users.noreply.github.com>
2022-03-30 21:04:10 +0200

    Implemented CardView using RecyclerView for Home page

--- app/src/main/java/com/example/tutorhub/Home.java
+++ app/src/main/java/com/example/tutorhub/Home.java
@@ -3,20 +3,60 @@ package com.example.tutorhub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
-
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Home extends AppCompatActivity {
+
+    private RecyclerView courseRV;
+    /*private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();*/
+
+    private ArrayList<CourseModel> courseModelArrayList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+        courseRV = findViewById(R.id.idRVCourse);
+
+        // here we have created new array list and added data to it.
+        courseModelArrayList = new ArrayList<>();
+        courseModelArrayList.add(new CourseModel("DSA in Java", 4, R.drawable.a));
+        courseModelArrayList.add(new CourseModel("Java Course", 3, R.drawable.b));
+        courseModelArrayList.add(new CourseModel("C++ COurse", 4, R.drawable.c));
+        courseModelArrayList.add(new CourseModel("DSA in C++", 4, R.drawable.a));
+        courseModelArrayList.add(new CourseModel("Kotlin for Android", 4, R.drawable.b));
+        courseModelArrayList.add(new CourseModel("Java for Android", 4, R.drawable.c));
+        courseModelArrayList.add(new CourseModel("HTML and CSS", 4, R.drawable.a));
+
+
+        // we are initializing our adapter class and passing our arraylist to it.
+        CourseAdapter courseAdapter = new CourseAdapter(this, courseModelArrayList);
+
+        // below line is for setting a layout manager for our recycler view.
+        // here we are creating vertical list so we will provide orientation as vertical
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        // in below two lines we are setting layoutmanager and adapter to our recycler view.
+        courseRV.setLayoutManager(linearLayoutManager);
+        courseRV.setAdapter(courseAdapter);
         //setContentView(R.layout.home);
 
         /*Button info = findViewById(R.id.info_button);
