29afe84b27027b0b01337950dc21968016e0b68c
TimStapert <82938308+TimStapert@users.noreply.github.com>
2022-04-03 17:17:34 +0200

    start with location implementation

--- app/src/main/java/com/example/tutorhub/Home.java
+++ app/src/main/java/com/example/tutorhub/Home.java
@@ -1,6 +1,10 @@
 package com.example.tutorhub;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -10,14 +14,71 @@ import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class Home extends AppCompatActivity {
 
+    private FusedLocationProviderClient fusedLocationClient;
+
+    private final int REQUEST_ACCESS_COARSE_LOCATION = 1;
+
+
+
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    return;
+                } else {
+                    Toast.makeText(this, "TutorHub has no location permissions!", Toast.LENGTH_LONG);
+                }
+            });
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.home);
+        setContentView(R.layout.home);
+
+        ActivityResultLauncher<String[]> locationPermissionRequest = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), result -> {
+                    Boolean coarseLocationGranted = result.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false);
+                    if (coarseLocationGranted != null && coarseLocationGranted) {
+                        // Only approximate location access granted.
+                    } else {
+                        // No location access granted.
+                        Toast.makeText(this, "TutorHub has no location permissions!", Toast.LENGTH_LONG);
+                    }
+                }
+        );
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        System.out.println("LAT: " + location.getLatitude());
+                        System.out.println("LON: " + location.getLongitude());
+                    }
+                }
+            });
+
+
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+        } else {
+
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+
 
         /*Button info = findViewById(R.id.info_button);
         info.setOnClickListener(new View.OnClickListener() {
