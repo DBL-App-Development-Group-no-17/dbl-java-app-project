6ddd5d044f3dbe24597c1e229b4aa3f07abe652f
KatiOverbeeke <k.overbeeke@student.tue.nl>
2022-04-03 17:02:15 +0200

    init firebase query on users

--- app/src/main/java/com/example/tutorhub/Home.java
+++ app/src/main/java/com/example/tutorhub/Home.java
@@ -1,22 +1,17 @@
 package com.example.tutorhub;
 
-import android.app.SearchManager;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.SearchView;
-import android.widget.Toast;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -30,7 +25,17 @@ public class Home extends AppCompatActivity {
 
     private ArrayList<CourseModel> courseModelArrayList;
 
-    DatabaseReference tutorsRef = mAuth.getReference("tutors");
+    Task<DataSnapshot> database = FirebaseDatabase.getInstance().getReference().child("users").get().addOnCompleteListener(
+            new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        String s = task.getResult().getValue().toString();
+                        String[] ss = s.split(", ");
+                    }
+                }
+            });
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
